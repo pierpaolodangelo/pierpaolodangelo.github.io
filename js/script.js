90c617ca38ea354bcdc3689cbf200aaf2f4b519a
Pierpaolo D'Angelo <pierpaolodangelo96@gmail.com>
2020-10-15 23:08:50 +0200

    Aggiunta possibilità di selezionare la data di cui visualizzare i dati

--- js/script.js
+++ js/script.js
@@ -1,31 +1,35 @@
 function loadData() {
     const request = new XMLHttpRequest();
-    request.open("get", "https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-json/dpc-covid19-ita-regioni-latest.json");
+    request.open("get", /*"https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-json/dpc-covid19-ita-regioni-latest.json"*/
+        "https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-json/dpc-covid19-ita-regioni.json");
     request.onload = () => {
         try {
             const json = JSON.parse(request.responseText);
-            populateData(json);
+            populateDataByDate(json);
 
         } catch (e) {
+            console.log(e);
             console.warn("Could not retrieve data from the json :(");
         }
     };
     request.send();
 }
 
-function populateData(json) {
-    for (var i in json) {
+function populateDataByDate(json) {
+    console.log(json);
+    var date = document.getElementById("date-input").value;
+    var newData = json.filter(el => el.data.startsWith(date));
+    console.log(newData);
+    var table = $('#table-body');
+    table.empty();
+    newData.forEach(element => {
         var row = `<tr>
-                        <td>${json[i].denominazione_regione}</td>
-                        <td>${json[i].dimessi_guariti}</td>
-                        <td>${json[i].deceduti}</td>
-                        <td>${json[i].totale_casi}</td>
-                        <td>${json[i].nuovi_positivi}</td>
-                    </tr> `
-        var table = $('#table-body');
+        <td>${element.denominazione_regione}</td>
+        <td>${element.dimessi_guariti}</td>
+        <td>${element.deceduti}</td>
+        <td>${element.totale_casi}</td>
+        <td>${element.nuovi_positivi}</td>
+        </tr>`
         table.append(row);
-
-    }
-
-
+    });
 }
