fbe1530148470bc6abb5d98c6c888742f72d51a6
Pierpaolo D'Angelo <pierpaolodangelo96@gmail.com>
2020-10-27 02:41:09 +0100

    introdotta funzione javascript per popolare box e refactoring

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-function loadData() {
+function loadData(callBackFunction) {
   const request = new XMLHttpRequest();
   request.open(
     "get",
@@ -7,7 +7,7 @@ function loadData() {
   request.onload = () => {
     try {
       const json = JSON.parse(request.responseText);
-      populateDataByDate(json);
+      callBackFunction(json);
     } catch (e) {
       console.log(e);
       console.warn("Could not retrieve data from the json :(");
@@ -16,14 +16,18 @@ function loadData() {
   request.send();
 }
 
-function populateDataByDate(json) {
+function populateByDate(json) {
   json.sort((a, b) => b.data.localeCompare(a.data));
   var date = document.getElementById("date-input").value;
   date = !date ? json[0].data : date;
   var newData = json.filter((el) => el.data.startsWith(date));
+  populateTable(newData);
+}
+
+function populateTable(data) {
   var table = $("#table-body");
   table.empty();
-  newData.forEach((element) => {
+  data.forEach((element) => {
     var row = `<tr>
         <td>${element.denominazione_regione}</td>
         <td>${element.dimessi_guariti}</td>
@@ -33,6 +37,21 @@ function populateDataByDate(json) {
         </tr>`;
     table.append(row);
   });
+}
+
+function filterByRegion(data) {
+  data.sort((a, b) => b.data.localeCompare(a.data));
+  var filtered = data.filter((el) => el.data.startsWith(data[0].data))
+  .filter((el) => el.denominazione_regione == document.getElementById("nome-regione").firstChild.data);
+    populateFields(filtered);
+}
+
+function populateFields(data) {
+  console.log(data);
+  document.getElementById("box-casi-totali").firstChild.data = data[0].totale_casi; 
+  document.getElementById("box-incremento-casi").firstChild.data = data[0].nuovi_positivi;
+  document.getElementById("box-deceduti").firstChild.data = data[0].deceduti;
+  document.getElementById("box-dimessi-guariti").firstChild.data = data[0].dimessi_guariti;
 }
 
 function displayName(name) {
